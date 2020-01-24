df9495392f4eb8afe31514ba865159c55521f8fa
arkinem <blazejgolinski@live.com>
2020-01-24 21:49:02 +0000

    initial project setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return <Container>Page</Container>;
+  }
 }
 
 export default App;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
